162ce6e5c936b05006d55b0466ca87cf0b28036a
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 22:21:29 +0300

    Ürün Bilgileri Güncellendi

--- 6sproducts/6sproduct35.php
+++ 6sproducts/6sproduct35.php
@@ -149,13 +149,14 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../6takımpage1.php" style="text-decoration: none; color: black;">6 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Liyon Masa & Efsane Sandalye</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-60" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(60)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span> 🛠️ 90x165 ölçü 6 kişilik
+                    🛠️ 95x190 ölçü 8 kişilik. <br>
 
                      <strong>Masa;</strong> <br>
                     
