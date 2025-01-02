c7a7aa218cd00e0b5b31e8ccc342280aea3d0039
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:28:33 +0300

    Ürün Bilgileri Güncellendi

--- 6sproducts/6sproduct6.php
+++ 6sproducts/6sproduct6.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../6takımpage1.php" style="text-decoration: none; color: black;">6 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Şile Masa Sandalye Takımı</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-31" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(31)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>💫Şıklığın ve fonksiyonelliğin buluşma noktası: Şile Masa Sandalye Takımı. Çift kanat açılır mekanizması ile hem kullanışlı hem de estetik! 💎🏡 <br>
 
                      <strong>Masa;</strong> <br>
                     
