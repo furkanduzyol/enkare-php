b9affd4ee1cbe22c67f67aa8ab06c22603ff88a2
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 20:29:03 +0300

    Ürün Bilgileri Güncellendi

--- 4sproducts/4sproduct3.php
+++ 4sproducts/4sproduct3.php
@@ -149,14 +149,14 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../4takımpage1.php" style="text-decoration: none; color: black;">4 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>X Ayaklı Gloss Masa & Metal Polo Sandalye</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 
                 <input type="number" id="quantity-3" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(3)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>Yenilikçi tasarım ve dayanıklılığın buluştuğu X ayaklı Gloss Masa ile metal polo sandalyeler, modern yaşam alanlarınızı şıklıkla donatıyor. Çift kanat açılır mekanizması sayesinde pratik kullanım sunan bu masa sandalye takımı, her davet için ideal. Metal dayanıklılığı ile uzun ömürlü bir şıklık arıyorsanız, bu set tam size göre! 🌟<br>
 
                      <strong>Masa;</strong> <br>
                     
