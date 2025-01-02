ef390d4e619fd6dabf78d5841c61bc79971e77ae
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:33:34 +0300

    Ürün Bilgileri Güncellendi

--- 6sproducts/6sproduct10.php
+++ 6sproducts/6sproduct10.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../6takımpage1.php" style="text-decoration: none; color: black;">6 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Yengeç Ayaklı Venüs Masa & Erciyes Sandalye</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-35" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(35)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>💫Şıklığın ve konforun buluşma noktası! Tek kanat açılır mekanizması ile Venüs Masa, her anınıza uyum sağlıyor. Gri renkte babyface kumaşla kaplı Erciyes Sandalyeler ise estetik ve rahatlığı bir arada sunuyor. ✨ <br>
 
                      <strong>Masa;</strong> <br>
                     
