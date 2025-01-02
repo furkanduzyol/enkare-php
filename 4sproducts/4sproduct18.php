d2318fe39fbde0e3687a487ecbff0751e3bd9a5f
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:07:39 +0300

    Ürün Bilgileri Güncellendi

--- 4sproducts/4sproduct18.php
+++ 4sproducts/4sproduct18.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../4takımpage1.php" style="text-decoration: none; color: black;">4 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Ladin Ağacı Kütük Masa & Zen Sandalye</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-18" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(18)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>NkareHome'dan size özel bir Zen deneyimi sunuyoruz: Doğanın sakinliği ve zarafetiyle tasarlanmış Ladin Ağacı Kütük Masa ve Zen Sandalyenin dayanıklı yapısıyla İç mekanınıza güven, doğallık ve huzur katmanın tam zamanı! 🌿✨ <br>
 
                      <strong>Masa;</strong> <br>
                     
