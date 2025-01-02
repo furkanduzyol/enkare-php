a65e85ad892bd4435a0b85b2518414bb36b31f72
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 20:40:30 +0300

    Ürün Bilgileri Güncellendi

--- 4sproducts/4sproduct8.php
+++ 4sproducts/4sproduct8.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../4takımpage1.php" style="text-decoration: none; color: black;">4 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Defne Masa Sandalye Takımı</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-8" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(8)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span> 💫 Zarafetin ve modern tasarımın mükemmel uyumu: Defne Masa Sandalye Takımı. Yuvarlak ahşap masa, sıcaklığıyla mekanınıza samimiyet katarken, metal ayaklı sandalyeler şıklığı ve dayanıklılığı bir araya getiriyor. Evinizde sofistike bir dokunuş üretmeye hazır olun! <br>
 
                      <strong>Masa;</strong> <br>
                     
