1126db83b612acdcb486ed8b31ee2f7b68b91227
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:22:09 +0300

    Ürün Bilgileri Güncellendi

--- 6sproducts/6sproduct2.php
+++ 6sproducts/6sproduct2.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../6takımpage1.php" style="text-decoration: none; color: black;">6 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Gürgen Ağacı Ayaklalı Masa ve Zarif Koltuklu Sandalye</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-27" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(27)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>Masa üstü parlak yüzey lak panel MDF ile tasarlanmış, masa ve sandalye ayakları gürgen ağacından elde edilmiş bu muazzam Kale Masa Sandalye Takımı ile salon, mutfak ve yemek odanızda stil ve konforu bir araya getirin! 💫. <br>
 
                      <strong>Masa;</strong> <br>
                     
