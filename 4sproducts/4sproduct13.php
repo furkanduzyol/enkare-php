d8c185a97423a5113d9ff32b841d0b2704533d0e
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 20:59:09 +0300

    Ürün Bilgileri Güncellendi

--- 4sproducts/4sproduct13.php
+++ 4sproducts/4sproduct13.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details"><h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../4takımpage1.php" style="text-decoration: none; color: black;">4 Kişilik İç Mekan Takımlar</a></h6>
                 
-                <h4>Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>EX Ayaklı Gloss Masa & Papatya Sandalye/h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-13" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(13)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>💫 Dayanıklı ve Estetik: X Ayaklı Gloss Masa, MDF malzemesi ve açılır mekanizmasıyla fonksiyonel bir şıklık sunuyor. Papatya Sandalye ile harika bir kombin! 🌼 <br>
 
                      <strong>Masa;</strong> <br>
                     
