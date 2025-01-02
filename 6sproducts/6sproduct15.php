a010c43d0c1479e17400c0762b05ac9a94151a36
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:40:30 +0300

    Ürün Bilgileri Güncellendi

--- 6sproducts/6sproduct15.php
+++ 6sproducts/6sproduct15.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../6takımpage1.php" style="text-decoration: none; color: black;">6 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Parlak Yüzey Kelebek Masa & Defne Sandalye & Polo Bank</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-40" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(40)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>💫 Enkarehome'dan zarafet ve kullanışlılık bir arada! Parlak Yüzey MDF Kelebek Masa, Defne Sandalye ve Polo Bank ile evinizin şıklığını tamamlayın. Siyah boyalı statik ayaklar ve babyface kumaş sandalyeyle rahatlığı hissedin. Detaylar için profilimizi ziyaret edin! ✨. <br>
 
                      <strong>Masa;</strong> <br>
                     
