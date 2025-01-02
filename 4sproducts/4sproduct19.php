817f1809928d794f592f605c656b0035f1bad4e0
Arda Toptaş <arda.toptas@hotmail.com>
2025-01-02 21:09:24 +0300

    Ürün Bilgileri Güncellendi

--- 4sproducts/4sproduct19.php
+++ 4sproducts/4sproduct19.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../4takımpage1.php" style="text-decoration: none; color: black;">4 Kişilik İç Mekan Takımlar</a></h6>
-                <h4>Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                <h4>Şile Masa Sandalye Takımı</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-19" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(19)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>nkareHome'un zarif ve kullanışlı ŞİLE masa ve sandalye takımı, babyface kumaş ve şık gri rengiyle dikkat çekiyor. Masanın çift kanat açılır mekanizmasıyla pratik kullanım sağlarken, sandalyelerdeki detaylar estetiği ön plana çıkarıyor. Modern tasarımıyla yaşam alanlarınıza şıklık katın! <br>
 
                      <strong>Masa;</strong> <br>
                     
