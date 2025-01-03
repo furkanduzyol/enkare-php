fc641c482ed4504de721aa32eafcfd49b01ee010
ayselcetin07 <103258056+ayselcetin07@users.noreply.github.com>
2025-01-03 15:27:08 +0300

    Update 8sproduct6.php

--- sbalkonbahce/8sproduct6.php
+++ sbalkonbahce/8sproduct6.php
@@ -149,13 +149,13 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../balkonbahce1.php" style="text-decoration: none; color: black;">Balkon ve Bahçe Mobilyaları</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                 <h4> Frezya Balkon Takımı  |</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-67" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(67)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>💫 Enkarehome'un göz alıcı Frezya Balkon Bahçe Takımı ve çay seti ile baharın keyfini çıkarın! Balkonunuzu veya bahçenizi şıklıkla donatın ve sevdiklerinizle unutulmaz anlar yaşayın. Detaylar için profilimizi ziyaret etmeyi unutmayın! 🌸🍵 <br>
 
                      <strong>Masa;</strong> <br>
                     
