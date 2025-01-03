2b846909c435a4747d6693537abde6339a80d599
ayselcetin07 <103258056+ayselcetin07@users.noreply.github.com>
2025-01-03 15:25:30 +0300

    Update 8sproduct4.php

--- sbalkonbahce/8sproduct4.php
+++ sbalkonbahce/8sproduct4.php
@@ -149,13 +149,14 @@ display: block;
             </div>
             <div class="single-pro-details">
                 <h6><a href="../4takımpage1.php" style="text-decoration: none; color: black;">Mağaza</a> / <a href="../balkonbahce1.php" style="text-decoration: none; color: black;">Balkon ve Bahçe Mobilyaları</a></h6>
-                <h4>Beyaz Elisa Masa Sandalye Takımı, 4 Kişilik Mutfak Masası, Teddy Sandalye, Yuvarlak Mdf Masa, 90 X 90</h4>
+                 <h4> Hezaren Rattan Bahçe Mobilyası</h4>
                 <h2 class="price">8.900,00 TL</h2>
                 <input type="number" id="quantity-65" value="1"> <!-- Input field for quantity -->
                 <button class="normal addCart" onclick="handleAddToCart(65)">Sepete Ekle</button>
                 
                 <h4>Ürün Açıklaması:</h4>
-                <span>Enkare Elisa Takım 4 Sandalye 1 Masadan oluşmaktadır. <br>
+                <span>Yazın keyfini bahçenizde Hezaren Rattan Bahçe Mobilyası ile çıkarın! 🌞✨ Doğal ve şık tasarımıyla bahçenize huzur getiren bu mobilya seti, rahatlığı ve dayanıklılığıyla öne çıkıyor. Doğanın içinde konforlu anlar yaşamak isteyenler için mükemmel bir seçim. 🪑🍃<br>
+
 
                      <strong>Masa;</strong> <br>
                     
